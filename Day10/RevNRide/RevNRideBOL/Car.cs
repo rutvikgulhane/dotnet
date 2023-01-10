3511c6980cef486aa5c19ff25be36c20705ff46f
rutvikgulhane <rutvikgulhane143@gmail.com>
2023-01-10 07:05:10 +0530

    RevNRide project beginning with n-tier arch

---
+++ Day10/RevNRide/RevNRideBOL/Car.cs
@@ -0,0 +1,49 @@
+﻿namespace RevNRideBOL;
+using System.ComponentModel.DataAnnotations;
+[Serializable]
+public class Car
+{
+    public string? Id { get; set; }
+    public string? Make { get; set; }
+    public string? Model { get; set; }
+    public int? ModelYear { get; set; }
+
+    public Car() { }
+
+    public Car(string id)
+    {
+        Id = id;
+    }
+
+    // override object.Equals
+    public override bool Equals(object obj)
+    {
+        //
+        // See the full list of guidelines at
+        //   http://go.microsoft.com/fwlink/?LinkID=85237
+        // and also the guidance for operator== at
+        //   http://go.microsoft.com/fwlink/?LinkId=85238
+        //
+
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        // TODO: write your implementation of Equals() here
+
+        return this.Id.Equals((obj as Car).Id);
+    }
+
+    // override object.GetHashCode
+    public override int GetHashCode()
+    {
+        // TODO: write your implementation of GetHashCode() here
+        return base.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+        return "Id->" + Id + " Make->" + Make + " Model->" + Model + " Model Year->" + ModelYear;
+    }
+}
---
+++ Day10/RevNRide/RevNRideDAL/CarDBManager.cs
@@ -0,0 +1,43 @@
+﻿namespace RevNRideDAL;
+using System;
+using System.Text.Json;
+using RevNRideBOL;
+
+public class CarDBManager
+{
+    public static List<Car> ReadCarsFromFile() {
+
+        string carString = File.ReadAllText
+        (@"/media/runner/DATA/IACSD AKURDI/DOT_NET/DN_Assignments/Day10/RevNRide/MOCK_DATA.json");
+        return JsonSerializer.Deserialize<List<Car>>(carString);
+    }
+
+    public static void AddCarsIntoFile(List<Car> carList, string fileName = @"/media/runner/DATA/IACSD AKURDI/DOT_NET/DN_Assignments/Day10/RevNRide/MOCK_DATA.json") {
+
+        File.WriteAllText(fileName, JsonSerializer.Serialize(carList));
+    }
+
+    public static bool AddCarIntoFile(Car car) {
+        
+        List<Car> cars = ReadCarsFromFile();
+        cars.Add(car);
+        AddCarsIntoFile(cars);
+        return true;
+    }
+
+    public static Car GetCarFromFile(string id) {
+
+        List<Car> cars = ReadCarsFromFile();
+        Car car = cars.FirstOrDefault(car => car.Id == id);
+
+        System.Console.WriteLine(car); 
+        int indexOfThatCar = cars.IndexOf(car);
+        if (car == null)
+        {
+            return null;
+        }
+        return car;
+    }
+
+    
+}
