4fce528cf6f593febc20c10e46d4537bb1c703dc
rutvikgulhane <rutvikgulhane143@gmail.com>
2023-01-15 18:56:43 +0530

    Making A WebAPI to connect with React

---
+++ Day15/HrManager/HrManagerDAL/ORM/DepartmentsContext.cs
@@ -0,0 +1,26 @@
+namespace HrManagerDAL.ORM;
+using Microsoft.EntityFrameworkCore;
+using HrManagerBOL.Entities;
+
+
+public class DepartmentsContext : DbContext
+{
+    public DbSet<Department> Departments { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string conString = @"server=localhost;port=3306;user=root; password=password;database=emp_info";
+        optionsBuilder.UseMySQL(conString);
+    }
+
+    // schema
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Department>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Department_name).IsRequired();
+        });
+        modelBuilder.Entity<Department>().ToTable("departments");
+    }
+}
